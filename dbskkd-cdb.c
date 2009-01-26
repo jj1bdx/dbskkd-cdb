3827accb2fe0557f89aef4ab8dd600016a50dfed
jj1bdx <jj1bdx@14f2559c-dab6-11dd-86c2-0f8696b7b6f9>
2009-01-26 00:04:38 +0000

    Reviewed and removed ambiguity from the distribution license
    git-svn-id: http://dbskkd-cdb.googlecode.com/svn/trunk@39 14f2559c-dab6-11dd-86c2-0f8696b7b6f9

--- dbskkd-cdb.c
+++ dbskkd-cdb.c
@@ -4,10 +4,8 @@
  * with Daniel J. Bernstein's cdb database
  *
  * Copyright (c) 1998-2009 by Kenji Rikitake. All rights reserved.
- * This code is distributed under
- * Creative Commons BSD License.
- * http://creativecommons.org/licenses/BSD/
  * See LICENSE of the distribution kit for the redistribution conditions.
+ * The copy of the LICENSE file is attached at the end of this code.
  */
 
 #include <fcntl.h>
@@ -200,3 +198,46 @@ int main(int argc, char *argv[])
 }
 
 /* end of program */
+
+/* 
+ * License statement for dbskkd-cdb
+ * 
+ * Copyright (c) 1998-2009 by Kenji Rikitake. All rights reserved.
+ * 
+ * This software is distributed under the terms of either
+ * 
+ * Creative Commons Attribution 3.0 Unported License
+ * http://creativecommons.org/licenses/by/3.0/ 
+ * 
+ * or
+ * 
+ * the Simplified BSD license, shown as follows:
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * For more information on the dbskkd-cdb project, please refer to 
+ * http://code.google.com/p/dbskkd-cdb/
+ * 
+ * [End of License statement for dbskkd-cdb]
+ */
